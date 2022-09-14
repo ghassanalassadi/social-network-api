8b541b50836dd87db561b38a859f92be5ba23545
ghassan <ghassanalassadi@outlook.com>
2022-09-14 19:21:15 +1000

    created models

---
+++ models/Thought.js
@@ -0,0 +1,58 @@
+const { Schema, model, Types } = require("mongoose");
+const moment = require('moment');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAt) => {moment(createdAtValue).format('DD MM, YYYY [at] hh:mm a')}
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+)
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
